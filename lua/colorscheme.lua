71493d87f85d00ef07c2ebfbf6e0b88ac6cdf5cd
Andrew Voss <andrewvoss212@gmail.com>
2024-07-10 11:27:27 -0500

    Add color scheme overrides

---
+++ lua/colorscheme.lua
@@ -0,0 +1,10 @@
+require("catppuccin").setup {
+    color_overrides = {
+        mocha = {
+            base = "#101010",
+            mantle = "#101010",
+            crust = "#101010",
+        },
+    },
+}
+
